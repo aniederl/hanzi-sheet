eb92edcc77ac45851cf9e303d36100c5eff75db9
Andreas Niederl <rico32@gmx.net>
2011-12-28 00:22:47 +0100

    generate hanzi lines with filter.py using context filter module

--- filter.py
+++ filter.py
@@ -14,6 +14,11 @@ from optparse import OptionParser
 usage  = "Usage: %prog input-file output-file"
 parser = OptionParser(usage)
 
+parser.add_option("-t", "--tex", action="store_true", dest="output_tex",
+    default=False)
+parser.add_option("-p", "--pinyin", action="store_true", dest="add_pinyin",
+    default=False)
+
 (options, args) = parser.parse_args()
 
 if len(args) < 2:
@@ -24,7 +29,10 @@ out_file = args[1]
 
 
 import re
-symbol_re = re.compile(r'^(\w)\s+', re.UNICODE)
+delim1 = '\t+|<br\s*/>'
+delim  = '(?:\t+|<br\s*/>|$)+'
+text   = '([\S ]+?)'
+symbol_re = re.compile(r'^(\w)' + delim + text + delim + text + delim, re.UNICODE)
 
 
 try:
@@ -35,8 +43,24 @@ except IOError as err:
 
 out_f = open(out_file, 'w')
 for line in in_f:
-  symbol = symbol_re.match(line)
-  if symbol:
-    out_f.write(symbol.group(1) + '\n')
+  line = re.sub('&quot;', '\'', line)
+  match = symbol_re.match(line)
+  if match:
+    symbol = match.group(1)
+    output = ''
+    if options.output_tex:
+      if options.add_pinyin:
+        pinyin = match.group(2)
+        pinyin = re.sub('<br\s*/>', '', pinyin)
+        desc   = match.group(3)
+        output = '\\linedesc{' + symbol + '}{' + pinyin + '}{' + desc + '}'
+      else:
+        output = '\\line{' + symbol + '}'
+    else:
+      output = symbol
+
+    output += '\n'
+    #print(output)
+    out_f.write(output)
 
 
