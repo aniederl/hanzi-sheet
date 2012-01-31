1b32c600d5d0bf430d7af26fd20b8dcce8eb5c0e
Andreas Niederl <rico32@gmx.net>
2012-01-31 13:45:47 +0100

    improve word handling

--- filter.py
+++ filter.py
@@ -32,54 +32,89 @@ import re
 delim1 = '\t+|<br\s*/>'
 delim  = '(?:\t+|<br\s*/>|$)+'
 text   = '([\S ]+?)'
-symbol_re = re.compile(r'^(\w)'    + delim + text + delim + text + delim, re.UNICODE)
-word_re   = re.compile(r'^(\w\w+)' + delim + text + delim + text + delim, re.UNICODE)
+symbol_re = re.compile(r'^(\w)'  + delim + text + delim + text + delim, re.UNICODE)
+word_re   = re.compile(r'^(\w+)' + delim + text + delim + text + delim, re.UNICODE)
 
 hanzi = set()
+words = set()
+word_list = list()
+readings        = dict()
+translations    = dict()
+processed_words = set()
 
 
 try:
   in_f  = open(in_file, 'r', encoding='utf-8')
+  out_f = open(out_file, 'w', encoding='utf-8')
 except IOError as err:
   print(err)
   sys.exit(err.errno)
 
 
-def printline(line, regex, out):
-  line   = re.sub('&quot;', '\'', line)
-  match  = regex.match(line)
+def printline(word, symbol, reading, translation):
   output = ''
+
+  if options.output_tex:
+    if options.add_reading:
+      output = '\\linedesc[' + word + '][' + symbol + \
+          '][' + reading + '][' + translation + ']'
+    else:
+      output = '\\line{' + symbol + '}'
+  else:
+    output = symbol
+
+  output += '\n'
+  out_f.write(output)
+
+
+for line in in_f:
+  line   = re.sub('&quot;', '\'', line)
+  match  = word_re.match(line)
 
   if match:
     word = match.group(1)
+    reading = match.group(2)
+    reading = re.sub('<br\s*/>', '', reading)
+    translation = match.group(3)
 
-    for symbol in word:
-      if symbol in hanzi:
-        continue
-
-      hanzi.add(symbol)
-
-      if options.output_tex:
-        if options.add_reading:
-          reading = match.group(2)
-          reading = re.sub('<br\s*/>', '', reading)
-          trans   = match.group(3)
-          output = '\\linedesc[' + word + '][' + symbol + \
-              '][' + reading + '][' + trans + ']'
-        else:
-          output = '\\line{' + symbol + '}'
-      else:
-        output = symbol
-
-      output += '\n'
-      out.write(output)
+    word_list.append(word)
+    words.add(word)
+    readings[word]     = reading
+    translations[word] = translation
 
 
-out_f = open(out_file, 'w', encoding='utf-8')
-for line in in_f:
-  printline(line, symbol_re, out_f)
+print_words = list()
+added_words = set()
+added_syms  = set()
+for phrase in word_list:
+  #print("phrase: " + phrase)
+  for step in range(1, len(phrase)+1):
+    for i in range(0, len(phrase)):
+      j    = i + step
+      word = phrase[i:j]
 
-in_f.seek(0)
-for line in in_f:
-  printline(line, word_re,   out_f)
+      #print("look: " + word)
+      if word in words and not word in added_words:
+        new = 0
+        for sym in word:
+          if not sym in added_syms:
+            new += 1
+
+        if new > 0:
+          #print("append: " + word + " - " + translations[word])
+          print_words.append(word)
+          added_words.add(word)
+
+          for sym in word:
+            added_syms.add(sym)
+
+
+for word in print_words:
+  for symbol in word:
+    if symbol in hanzi:
+      #print(symbol + " already done")
+      continue
+
+    hanzi.add(symbol)
+    printline(word, symbol, readings[word], translations[word])
 
